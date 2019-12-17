d60f38fdd7a1b779c02c438baeda07c6171e8856
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-17 16:47:10 -0300

    metodo updateStatus

--- spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
@@ -3,12 +3,17 @@ package com.springcurso.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springcurso.domain.Request;
+import com.springcurso.domain.enums.RequestState;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
     public List<Request> findAllByOwnerId(Long id);
+    
+    @Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+    public Request updateStatus(Long id, RequestState state);
 }
