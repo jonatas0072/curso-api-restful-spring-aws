dea97efd86268e312abf15552859e5eac9aca116
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-17 16:37:26 -0300

    Alterando RequestRepository, e Criando metodos no RequestStageRepository

--- spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
@@ -1,12 +1,14 @@
 package com.springcurso.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.springcurso.domain.Request;
 
 @Repository
-public interface RequestRepository extends JpaRepository<Request, Long>{
-    
-    
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    public List<Request> findAllByOwnerId(Long id);
 }
--- spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
@@ -1,9 +1,20 @@
 package com.springcurso.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RequestStageRepository extends JpaRepository<RequestStageRepository, Long>{
+import com.springcurso.domain.Request;
+import com.springcurso.domain.RequestStage;
 
+@Repository
+public interface RequestStageRepository
+        extends JpaRepository<RequestStageRepository, Long> {
+
+        public List<RequestStage> findAllByRequestId(Long Id);
+        
+        @Query("UPDATE Request SET state = ?2 WHERE id = ?1")
+        public Request updateStatus(Long id, RequestStage state);
 }
