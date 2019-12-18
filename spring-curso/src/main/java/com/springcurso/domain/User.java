2f26af449e6bba21f7cbca56e430eb6f3082f479
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-18 09:40:43 -0300

    Remoção do lombok

--- spring-curso/src/main/java/com/springcurso/domain/User.java
+++ spring-curso/src/main/java/com/springcurso/domain/User.java
@@ -16,20 +16,27 @@ import javax.persistence.Table;
 
 import com.springcurso.domain.enums.Role;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "owner")
 public class User implements Serializable {
 
     private static final long serialVersionUID = -8032599361046926793L;
+
+    public User() {
+        super();
+    }
+
+    public User(Long id, String name, String email, String password, Role role,
+            List<Request> requests, List<RequestStage> stages) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.requests = requests;
+        this.stages = stages;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -53,5 +60,68 @@ public class User implements Serializable {
 
     @OneToMany(mappedBy = "user")
     private List<RequestStage> stages = new ArrayList<RequestStage>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public List<Request> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<Request> requests) {
+        this.requests = requests;
+    }
+
+    public List<RequestStage> getStages() {
+        return stages;
+    }
+
+    public void setStages(List<RequestStage> stages) {
+        this.stages = stages;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", email=" + email
+                + ", password=" + password + ", role=" + role + ", requests="
+                + requests + ", stages=" + stages + "]";
+    }
 
 }
