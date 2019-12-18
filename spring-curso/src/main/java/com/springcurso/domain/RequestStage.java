f8bd99e37c2803204b26bbf2a55cd76b363877bc
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-18 09:45:53 -0300

    Remoção do lombok

--- spring-curso/src/main/java/com/springcurso/domain/RequestStage.java
+++ spring-curso/src/main/java/com/springcurso/domain/RequestStage.java
@@ -18,20 +18,26 @@ import javax.persistence.TemporalType;
 
 import com.springcurso.domain.enums.RequestState;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "request_stage")
 public class RequestStage implements Serializable {
 
     private static final long serialVersionUID = -7283598516608265794L;
+
+    public RequestStage() {
+        super();
+    }
+
+    public RequestStage(Long id, String description, Date realizationDate,
+            RequestState state, Request request, User user) {
+        super();
+        this.id = id;
+        this.description = description;
+        this.realizationDate = realizationDate;
+        this.state = state;
+        this.request = request;
+        this.user = user;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -56,4 +62,58 @@ public class RequestStage implements Serializable {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getRealizationDate() {
+        return realizationDate;
+    }
+
+    public void setRealizationDate(Date realizationDate) {
+        this.realizationDate = realizationDate;
+    }
+
+    public RequestState getState() {
+        return state;
+    }
+
+    public void setState(RequestState state) {
+        this.state = state;
+    }
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestStage [id=" + id + ", description=" + description
+                + ", realizationDate=" + realizationDate + ", state=" + state
+                + ", request=" + request + ", user=" + user + "]";
+    }
 }
