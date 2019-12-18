73e33d31e37bcbf91d81523c6c8a491e1e1e437f
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-18 13:19:38 -0300

    troca de User para USUARIO

--- spring-curso/src/main/java/com/springcurso/repository/UserRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/UserRepository.java
@@ -1,16 +1,12 @@
 package com.springcurso.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.springcurso.domain.User;
+import com.springcurso.domain.Usuario;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<Usuario, Long>{
+   
     
-    @Query("SELECT FROM User WHERE email = ?1 AND password = ?2")
-    public Optional<User> login(String email, String password);
 }
