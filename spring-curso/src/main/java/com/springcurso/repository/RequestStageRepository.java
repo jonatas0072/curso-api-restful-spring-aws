52f585a79f2970f554d95fc9aca1220d837aefea
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-18 13:19:14 -0300

    identação e removo de metodo

--- spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
@@ -1,7 +1,5 @@
 package com.springcurso.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +7,6 @@ import com.springcurso.domain.RequestStage;
 
 @Repository
 public interface RequestStageRepository
-        extends JpaRepository<RequestStageRepository, Long> {
-
-        public List<RequestStage> findAllByRequestId(Long Id);
+        extends JpaRepository<RequestStage, Long> {
         
 }
