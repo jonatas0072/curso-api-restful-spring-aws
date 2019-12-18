1c7880a69341250e8fc0d4025e43cd0672a40be9
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-18 09:42:29 -0300

    Correção

--- spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
@@ -3,12 +3,9 @@ package com.springcurso.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.springcurso.domain.Request;
 import com.springcurso.domain.RequestStage;
-import com.springcurso.domain.enums.RequestState;
 
 @Repository
 public interface RequestStageRepository
