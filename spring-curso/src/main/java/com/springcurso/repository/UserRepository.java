6a12b73ea4253c540b99d8174a5ea57e9aba4639
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-19 10:15:25 -0300

    Criando classe de Testes, e adptando ela para funcionar corretamente

--- spring-curso/src/main/java/com/springcurso/repository/UserRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/UserRepository.java
@@ -1,12 +1,17 @@
 package com.springcurso.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springcurso.domain.Usuario;
 
 @Repository
-public interface UserRepository extends JpaRepository<Usuario, Long>{
-   
-    
+public interface UserRepository extends JpaRepository<Usuario, Long> {
+
+    @Query(value = "SELECT u.email, u.password, u.id,u.name, u.role FROM usuario u WHERE email = ?1 AND password = ?2", nativeQuery = true)
+    Optional<Usuario> login(String email, String password);
+
 }
---
+++ spring-curso/src/test/java/com/springcurso/repository/UserRepositoryTests.java
@@ -0,0 +1,79 @@
+package com.springcurso.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springcurso.domain.Request;
+import com.springcurso.domain.RequestStage;
+import com.springcurso.domain.Usuario;
+import com.springcurso.domain.enums.Role;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void aSaveTest() {
+        Usuario user = new Usuario(1L, "Jonatas",
+                "jonatas.macedo@idtrust.com.br", "123", Role.ADMINISTRATOR,
+                new ArrayList<Request>(), new ArrayList<RequestStage>());
+
+        Usuario createdUser = userRepository.save(user);
+
+        Usuario findById = userRepository.findById(createdUser.getId()).get();
+
+        assertThat(createdUser.getId()).isEqualTo(findById.getId());
+    }
+
+    @Test
+    public void bUpdateTest() {
+        Usuario user = new Usuario(1L, "Jonatas Macedo",
+                "jonatas.macedo@idtrust.com.br", "3245678", Role.ADMINISTRATOR,
+                new ArrayList<Request>(), new ArrayList<RequestStage>());
+        Usuario updatedUser = userRepository.saveAndFlush(user);
+
+        assertThat(updatedUser.getName()).isEqualTo("Jonatas Macedo");
+    }
+
+    @Test
+    public void cFindByIdTest() {
+        Optional<Usuario> result = userRepository.findById(1L);
+        Usuario user = result.get();
+
+        assertThat(user.getPassword()).isEqualTo("3245678");
+
+    }
+
+    @Test
+    public void dListTest() {
+        List<Usuario> users = userRepository.findAll();
+
+        assertThat(users.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void eLoginTest() {
+        Optional<Usuario> result = userRepository
+                .login("jonatas.macedo@idtrust.com.br", "3245678");
+
+        Optional<Usuario> findById = userRepository
+                .findById(result.get().getId());
+
+        assertThat(result.get().getId()).isEqualTo(findById.get().getId());
+    }
+}
