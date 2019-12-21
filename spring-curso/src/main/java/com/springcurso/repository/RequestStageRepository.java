108151cb2ec61d20aac881273561a9af4f2db225
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-21 11:42:30 -0300

    metodo ainda nao necessario, no esquema do projeto

--- spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestStageRepository.java
@@ -1,5 +1,7 @@
 package com.springcurso.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.springcurso.domain.RequestStage;
 @Repository
 public interface RequestStageRepository
         extends JpaRepository<RequestStage, Long> {
+
+   // List<RequestStage> findAllRequestId(long id);
         
 }
