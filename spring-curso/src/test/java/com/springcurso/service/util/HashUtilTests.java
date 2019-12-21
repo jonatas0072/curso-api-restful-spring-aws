6d7d97302d6d4b77f3ad96d2d8f84e81c0865cdb
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-21 11:40:51 -0300

    Implementando test na geracao de Hash no password

---
+++ spring-curso/src/test/java/com/springcurso/service/util/HashUtilTests.java
@@ -0,0 +1,21 @@
+package com.springcurso.service.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class HashUtilTests {
+
+    @Test
+    public void getSecureHashTest() {
+        String hash = HashUtil.getSecureHash("123");
+        System.out.println("CODIGO " + hash);
+        assertThat(hash.length()).isEqualTo(64);
+    }
+
+}
