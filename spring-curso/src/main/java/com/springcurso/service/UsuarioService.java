81a06699d2707f35f838da90dcfdc3bfbe4b92af
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-21 11:41:44 -0300

    implementacao de metodos de CRUD

---
+++ spring-curso/src/main/java/com/springcurso/service/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.springcurso.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcurso.domain.Usuario;
+import com.springcurso.repository.UserRepository;
+import com.springcurso.service.util.HashUtil;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Usuario save(Usuario usuario) {
+        String hash = HashUtil.getSecureHash(usuario.getPassword());
+        usuario.setPassword(hash);
+        Usuario createdUsuario = userRepository.save(usuario);
+        return createdUsuario;
+    }
+
+    // update
+    public Usuario update(Usuario usuario) {
+        String hash = HashUtil.getSecureHash(usuario.getPassword());
+        usuario.setPassword(hash);
+
+        Usuario updatedUser = userRepository.saveAndFlush(usuario);
+        return updatedUser;
+    }
+
+    // get
+    public Usuario findById(Long id) {
+        Optional<Usuario> result = userRepository.findById(id);
+        return result.get();
+    }
+
+    // list
+    public List<Usuario> listAll() {
+        List<Usuario> usuarios = userRepository.findAll();
+        return usuarios;
+    }
+
+    // login
+    public Usuario login(String email, String password) {
+        password = HashUtil.getSecureHash(password);
+
+        Optional<Usuario> result = userRepository.login(email, password);
+        return result.get();
+    }
+}
