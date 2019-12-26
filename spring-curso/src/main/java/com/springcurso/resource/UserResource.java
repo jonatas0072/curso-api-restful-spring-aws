938aaf45514802640db6ed303d8dac83eb2d6280
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2019-12-26 13:10:02 -0300

    Criacao dos metodos HTTP no Resource do Usuario

---
+++ spring-curso/src/main/java/com/springcurso/resource/UserResource.java
@@ -0,0 +1,62 @@
+package com.springcurso.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springcurso.domain.Usuario;
+import com.springcurso.dto.UsuarioLoginDto;
+import com.springcurso.service.UsuarioService;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserResource {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> save(@RequestBody Usuario usuario) {
+        Usuario creadedUsuario = usuarioService.save(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creadedUsuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> update(@PathVariable(name = "id") Long id,
+            @RequestBody Usuario usuario) {
+        usuario.setId(id);
+        Usuario updateUsuario = usuarioService.update(usuario);
+        return ResponseEntity.ok(updateUsuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable(name = "id") Long id) {
+        Usuario usuario = usuarioService.findById(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listAll() {
+        List<Usuario> usuarios = usuarioService.listAll();
+        return ResponseEntity.ok(usuarios);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Usuario> login(
+            @RequestBody UsuarioLoginDto usuarioLogin) {
+        Usuario loggedUsuario = usuarioService.login(usuarioLogin.getEmail(),
+                usuarioLogin.getEmail());
+        return ResponseEntity.ok(loggedUsuario);
+
+    }
+
+}
