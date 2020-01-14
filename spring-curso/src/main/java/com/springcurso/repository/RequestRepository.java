24e6791c6f0ea174a91c9f8919bf590d8fb6aed6
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2020-01-14 14:51:31 -0300

    Adicionando metodo que busca por paginas, no repository

--- spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
@@ -2,6 +2,8 @@ package com.springcurso.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import com.springcurso.domain.Request;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    Page<Request> findAllByUserId(Long id, Pageable pageable);
 
     List<Request> findAllByUserId(Long id);
 
