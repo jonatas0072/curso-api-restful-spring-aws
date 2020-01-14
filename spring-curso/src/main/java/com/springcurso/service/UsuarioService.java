d15c81bf5d9b2559a4b391bfe20c98a55ea67dff
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2020-01-14 13:40:37 -0300

    remoção de comentarios e novo metodo que faz paginação soibre demanda

--- spring-curso/src/main/java/com/springcurso/service/UsuarioService.java
+++ spring-curso/src/main/java/com/springcurso/service/UsuarioService.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.springcurso.domain.Usuario;
 import com.springcurso.exception.NotFoundException;
+import com.springcurso.model.PageModel;
+import com.springcurso.model.PageRequestModel;
 import com.springcurso.repository.UserRepository;
 import com.springcurso.service.util.HashUtil;
 
@@ -24,7 +29,6 @@ public class UsuarioService {
         return createdUsuario;
     }
 
-    // update
     public Usuario update(Usuario usuario) {
         String hash = HashUtil.getSecureHash(usuario.getPassword());
         usuario.setPassword(hash);
@@ -33,20 +37,26 @@ public class UsuarioService {
         return updatedUser;
     }
 
-    // get
     public Usuario findById(Long id) {
         Optional<Usuario> result = userRepository.findById(id);
         return result.orElseThrow(() -> new NotFoundException(
                 "Não a usuario com este ID = " + id));
     }
 
-    // list
     public List<Usuario> listAll() {
         List<Usuario> usuarios = userRepository.findAll();
         return usuarios;
     }
 
-    // login
+    public PageModel<Usuario> listAllOnLazyMode(PageRequestModel prm) {
+        Pageable pageable = PageRequest.of(prm.getPage(), prm.getSize());
+        Page<Usuario> page = userRepository.findAll(pageable);
+        
+        PageModel<Usuario> pm = new PageModel<Usuario>((int)page.getTotalElements(), page.getSize(), page.getTotalPages(), page.getContent());
+        
+        return pm;
+    }
+
     public Usuario login(String email, String password) {
         String password_hash = HashUtil.getSecureHash(password);
 
@@ -58,4 +68,5 @@ public class UsuarioService {
         return result.get();
 
     }
+
 }
