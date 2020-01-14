0b791719de6e6453bbba7248240f41296a1fe6d2
JonatasMacedo <jonatasfernandoaraujomacedo@gmail.com>
2020-01-14 09:48:37 -0300

    Remoção da query sql native da interface repository

--- spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
+++ spring-curso/src/main/java/com/springcurso/repository/RequestRepository.java
@@ -3,21 +3,12 @@ package com.springcurso.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.springcurso.domain.Request;
-import com.springcurso.domain.enums.RequestState;
 
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
-
-    @Transactional(readOnly = false)
-    @Modifying
-    @Query(value = "UPDATE Request SET state = ?2 WHERE id = ?1", nativeQuery = true)
-    Request updateStatus(Long id, RequestState state);
 
     List<Request> findAllByUserId(Long id);
 
